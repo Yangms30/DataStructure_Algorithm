23be0479c96edea03f51e5e1bdaad4e25d94dce3
Yang <86237798+Yangms30@users.noreply.github.com>
2024-12-10 09:41:03 +0900

    Update best-time-to-buy-and-sell-stock.py
    전략 추가

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,3 +1,8 @@
+# 전략 : 앞에서부터 돌아가면서 제일 작은 변수 와 profit을 계산한다
+# 앞에서부터 min 보다 작으면 얘를 min에 넣고 아니면 min을 뺀 값을 profit에 저장한다
+# min보다 큰 숫자들은 profit에만 값을 저장하는 것에 의미가 있고 다시 볼 필요가 없음
+
+
 class Solution:
     def maxProfit(self,prices):
         if len(prices) < 2 or not prices : return 0
