9ca3d723be7057c64b8a020216e08838fe07472b
Yang <86237798+Yangms30@users.noreply.github.com>
2024-12-10 09:40:06 +0900

    Time: 43 ms (79.62%) | Memory: 26.4 MB (31.22%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,13 @@
 class Solution:
     def maxProfit(self,prices):
-        if not prices or len(prices) < 2:
-            return 0
-        min_price = float('inf')  # Set initial minimum price to infinity
-        max_profit = 0  # Initialize maximum profit as 0
-        for price in prices:
-        # Update the minimum price seen so far
-            if price < min_price:
-                min_price = price
-        # Calculate the profit if sold today and update the maximum profit
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-
-        return max_profit
+        if len(prices) < 2 or not prices : return 0
+        minimum = float('inf')
+        profit = 0
+        for i in range(len(prices)):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            else:
+                im = prices[i] - minimum
+                if(im > profit):
+                    profit = im
+        return profit
